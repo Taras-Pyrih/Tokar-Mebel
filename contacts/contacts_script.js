be5cabf259860f1239c020a519136f652c594c0c
Taras-Pyrih <pyrigtyu@gmail.com>
2022-01-06 12:38:05 +0200

    contacts added

---
+++ contacts/contacts_script.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+  let languageSelection = document.querySelector(".language-selection");
+  let languageList = document.querySelector(".language-list");
+  let dropdownListArrowOneImg = document.querySelector(".dropdown-list-arrow-one img");
+
+  languageSelection.addEventListener("click", function () {
+    languageList.classList.toggle("open");
+    dropdownListArrowOneImg.classList.toggle("active");
+  });
+
+  let languageListItems = document.querySelectorAll(".language-list__item");
+  
+  for (let i = 0; i < languageListItems.length; i++) {
+    languageListItems[i].addEventListener("click", function () {
+      let selectedLanguage = this.cloneNode(true);
+      languageSelection.firstElementChild.replaceWith(selectedLanguage);
+    });
+  }
+  
+  let map = document.querySelector(".map");
+  let iframe = document.querySelector(".map iframe");
+  
+  map.style.width = "100%";
+  map.style.height = `${parseInt(getComputedStyle(map).height) - 4}px`;
+  iframe.style.width = "100%";
+
+  let leftArrowFirst = document.querySelector(".last-articles-slider-block .arrow_left");
+  let rightArrowFirst = document.querySelector(".last-articles-slider-block .arrow_right");
+
+  $(".last-articles-slider").slick({
+    slidesToShow: 3,
+    prevArrow: leftArrowFirst,
+    nextArrow: rightArrowFirst,
+    responsive: [
+      {
+        breakpoint: 1481,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 951,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  let currentArticle = document.querySelector(".current-article");
+  currentArticle.innerHTML = "1/" + $(".last-articles-slider").slick("getSlick").slideCount;
+
+  $(".last-articles-slider").on("beforeChange", function(event, slick, currentSlide, nextSlide) {       
+    currentArticle.innerHTML = `${nextSlide + 1}/${slick.slideCount}`;
+  });
+  
+  let hamburgerMenu = document.querySelector(".hamburger-menu");
+  let navigationTop = document.querySelector(".header-content-bottom-row .navigation");
+  
+  hamburgerMenu.addEventListener("click", function () {
+    this.classList.toggle("active");
+    navigationTop.classList.toggle("active");
+  });
+});
